30f18c51d47ee03577eea7b33abf48d8c53e0848
qiaolunzhang <qiaolunzhang@gmail.com>
2019-02-17 12:57:10 +0100

    [utils] functions about task dependency
    fuctions:
    getTaskIndex: map taskId to the index id in the class
    addDependency: add dependency between two tasks
    removeDependency: after one task finished, remove the dependencies on it
    checkDependency: check if all the dependencies of a task is met
    
    todo: getTaskToSubmit:
    after a task finished, return the tasks that met the dependencies
    requirement now

--- src/edu/boun/edgecloudsim/utils/TaskBasedTask.java
+++ src/edu/boun/edgecloudsim/utils/TaskBasedTask.java
@@ -2,8 +2,91 @@ package edu.boun.edgecloudsim.utils;
 
 public class TaskBasedTask {
 	private int numSubTask;
+	private int[] tasks;
+	private int[] submitted;
+	/*
+	 * dependency[0][1] = 1 means task 0 is dependent on task 1
+	 * 	 0 1
+	 * 0 
+	 * 1
+	 */
 	private int[][] dependency;
-	public TaskBasedTask(int num) {
-		numSubTask = 0;
+	/*
+	 * initial: if dependency[0][1] = 1, dependency_met[0][1] = 0
+	 * 			otherwise, dependency_met[0][1] = 1
+	 * dependency_met[0][1] = 1 means dependency for 0 is met considering 1
+	 */
+	private int[][] dependency_met;
+	
+	public TaskBasedTask(int num, int[] taskList) {
+		numSubTask = num;
+		tasks = new int[numSubTask];
+		for (int i=0; i<numSubTask; i++) {
+			tasks[i]= taskList[i]; 
+		}
+		/*
+		 * Initialize the task with no dependency
+		 * dependencies are added later 
+		 */
+		dependency = new int[numSubTask][numSubTask];
+		dependency_met = new int[numSubTask][numSubTask];
+		for (int i=0; i < numSubTask; i++) {
+			for (int j=0; j<numSubTask; j++) {
+				dependency[i][j] = 0;
+				dependency_met[i][j]= 1; 
+			}
+		}
 	}
+	
+	/*
+	 * given a taskId, return the task_index`
+	 */
+	public int getTaskIndex(int taskId) {
+		int task_index = 0;
+		for (int i=0; i<numSubTask; i++) {
+			if (tasks[i] == taskId) {
+				task_index = i;
+				break;
+			}
+		}
+		return task_index;
+	}
+
+	/*
+	 * Add a dependency dependencyTaskId for task taskId
+	 */
+	public void addDependency(int taskId, int dependencyTaskId) {
+		int task_index = getTaskIndex(taskId);
+		int dependency_index = getTaskIndex(dependencyTaskId);
+		dependency[task_index][dependency_index] = 1;
+		dependency_met[task_index][dependency_index] = 0;
+	}
+	
+	/*
+	 * remove dependency when a task finished
+	 */
+	public void removeDependency(int taskId) {
+		int task_index = getTaskIndex(taskId);
+		for (int i=0; i<numSubTask; i++) {
+			dependency_met[i][task_index] = 1;
+		}
+	}
+
+	public boolean checkDependency(int taskId) {
+		int task_index = getTaskIndex(taskId);
+		boolean flag = true;
+		for (int i=0; i<numSubTask; i++) {
+			if (dependency_met[task_index][i] == 0) {
+				flag = false;
+			}
+		}
+
+		return flag;
+	}
+/*	
+	public int[] getTaskToSubmit(int taskFinishedId) {
+	}
+	*/
+
+
 }
