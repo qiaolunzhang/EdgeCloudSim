b477210d6b8a35332ecf9243918ece9bd57b7bd1
qiaolunzhang <qiaolunzhang@gmail.com>
2019-02-20 15:57:01 +0100

    [sample_example5] add sample_example5 an example with multi-task
    but with this example, no task is finished, but with the strategy in
    sample_example3, with a 71%, so I need to check it

--- src/edu/boun/edgecloudsim/applications/sample_app5/MainApp.java
+++ src/edu/boun/edgecloudsim/applications/sample_app5/MainApp.java
@@ -1,8 +1,18 @@
 package edu.boun.edgecloudsim.applications.sample_app5;
 
-import org.cloudbus.cloudsim.Log;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
+import org.cloudbus.cloudsim.Log;
+import org.cloudbus.cloudsim.core.CloudSim;
+
+import edu.boun.edgecloudsim.core.ScenarioFactory;
+import edu.boun.edgecloudsim.core.SimManager;
+import edu.boun.edgecloudsim.core.SimSettings;
 import edu.boun.edgecloudsim.utils.SimLogger;
+import edu.boun.edgecloudsim.utils.SimUtils;
 
 public class MainApp {
 
@@ -32,7 +42,71 @@ public class MainApp {
 			edgeDevicesFile = "scripts/sample_app3/config/edge_devices.xml";
 			outputFolder = "sim_results/ite" + iterationNumber;
 		}
+		
+		// Load settings from configuration file
+		SimSettings SS = SimSettings.getInstance();
+		if (SS.initialize(configFile, edgeDevicesFile, applicationsFile) == false) {
+			SimLogger.printLine("cannot initialize simulation settings!");
+			System.exit(0);
+		}
+		if (SS.getFileLoggingEnabled()) {
+			SimLogger.enableFileLog();
+			SimUtils.cleanOutputFolder(outputFolder);
+		}
 
+		DateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date SimulationStartDate = Calendar.getInstance().getTime();
+		String now = df.format(SimulationStartDate);
+		SimLogger.printLine("Simulation started at " + now);
+		SimLogger.printLine("-------------------------------------------------------");
+
+		for (int j=SS.getMinNumOfMobileDev(); j<=SS.getMinNumOfMobileDev(); j++) 
+		{
+			for (int k=0; k<SS.getSimulationScenarios().length; k++)
+			{
+				for (int i=0; i<SS.getOrchestratorPolicies().length; i++)
+				{
+					String simScenario = SS.getSimulationScenarios()[k];
+					String orchestratorPolicy = SS.getOrchestratorPolicies()[i];
+					Date ScenarioStartDate = Calendar.getInstance().getTime();
+					now = df.format(ScenarioStartDate);
+					
+					SimLogger.printLine("Scenario started at " + now);
+					SimLogger.printLine("Scenario: " + simScenario + " - Policy: " + orchestratorPolicy + " - #iteration: " + iterationNumber);
+					SimLogger.printLine("Duration: " + SS.getSimulationTime()/3600 + " hour(s) - Posisson: " + SS.getTaskLookUpTable()[0][2] + " - #devices: " + j);
+					SimLogger.getInstance().simStarted(outputFolder, "SIMRESULT_" + simScenario + "_" + orchestratorPolicy + "_" + j + "DEVICES");
+					
+					try
+					{
+						int num_user = 2;
+						Calendar calendar = Calendar.getInstance();
+						boolean trace_flag = false;
+						
+						// Initialize the  CloudSim library
+						CloudSim.init(num_user, calendar, trace_flag, 0.01);
+
+						// Generate EdgeCloudsim Scenario Factory
+						ScenarioFactory sampleFactory = new SampleScenarioFactory(j, SS.getSimulationTime(), orchestratorPolicy, simScenario);
+						
+						// Generate Edgecloudsim Simulation Manager
+						SimManager manager = new SimManager(sampleFactory, j, simScenario, orchestratorPolicy);
+						
+						// Start simulation
+						manager.startSimulation();
+					}
+					catch (Exception e) {
+						SimLogger.printLine("The simulation has been terminated due to an unexpected error");
+						e.printStackTrace();
+						System.exit(0);
+					}
+					
+					Date ScenarioEndDate = Calendar.getInstance().getTime();
+					now = df.format(ScenarioEndDate);
+					SimLogger.printLine("Scenario finished at " + now + ". It took " + SimUtils.getTimeDifference(ScenarioStartDate, ScenarioEndDate));
+					SimLogger.printLine("-----------------------------------------------------------------");
+				}
+			}
+		}
 	}
 
 }
