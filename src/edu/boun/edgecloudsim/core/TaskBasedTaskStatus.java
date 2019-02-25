f21442f6e49bc1f756516f1dce0a439829a76ed4
qiaolunzhang <qiaolunzhang@gmail.com>
2019-02-25 21:04:52 +0100

    [core/TaskBasedTaskStatus utils/TaskBasedTask] add two class to log the status of the taskBasedTask TaskBasedTaskStatus logs the status of all sub-task TaskBasedTask is the class to represent the status of a taskBasedTask

---
+++ src/edu/boun/edgecloudsim/core/TaskBasedTaskStatus.java
@@ -0,0 +1,64 @@
+package edu.boun.edgecloudsim.core;
+
+import java.util.*;
+
+import edu.boun.edgecloudsim.utils.TaskBasedTask;
+
+public class TaskBasedTaskStatus {
+	
+
+	private static TaskBasedTaskStatus instance = null;
+	private int numTaskBasedTask;
+	private Map<Integer, TaskBasedTask> taskBasedTaskMap;
+	private Map<Integer, Integer> mKeyMap;
+	
+	private TaskBasedTaskStatus() {
+		taskBasedTaskMap = new HashMap<Integer, TaskBasedTask>();
+		mKeyMap = new HashMap<Integer, Integer>();
+	}
+	
+	public TaskBasedTaskStatus getInstance() {
+		if (instance == null) {
+			instance = new TaskBasedTaskStatus();
+		}
+		return instance;
+	}
+	
+	public void addTaskBasedTask(int num_subtask, int taskBasedTaskId) {
+		taskBasedTaskMap.put(taskBasedTaskId, new TaskBasedTask(num_subtask, taskBasedTaskId));
+	}
+	
+	/**
+	 * @param id_subTask_list: the list of taskPropertyId
+	 * @param taskBasedTaskId: the id of taskBasedTask
+	 */
+	public void addSubTaskIdList(int[] id_subTask_list, int taskBasedTaskId) {
+		for (int i=0; i<id_subTask_list.length; i++) {
+			mKeyMap.put(id_subTask_list[i], taskBasedTaskId);
+		}
+		taskBasedTaskMap.get(taskBasedTaskId).addSubTaskIdList(id_subTask_list);
+	}
+	
+	public void addDependency(int id, int id_dependency, int taskBasedTaskId) {
+		taskBasedTaskMap.get(taskBasedTaskId).addDependency(id, id_dependency);
+	}
+	
+	public boolean checkReadySubmit(int taskPropertyId) {
+		//taskBasedTaskMap.get(task)
+		int taskBasedTaskId = mKeyMap.get(taskPropertyId);
+		boolean result = taskBasedTaskMap.get(taskBasedTaskId).checkReadySubmit(taskPropertyId);
+		return result;
+	}
+	
+	public List<Integer> getTaskSubmit(int taskPropertyId) {
+		int taskBasedTaskId = mKeyMap.get(taskPropertyId);
+		List<Integer> newTaskList = taskBasedTaskMap.get(taskBasedTaskId).getTaskToSubmit(taskPropertyId);
+		return newTaskList;
+	}
+	
+	public boolean checkTaskBasedTaskEnd(int taskPropertyId) {
+		int taskBasedTaskId = mKeyMap.get(taskPropertyId);
+		boolean result = taskBasedTaskMap.get(taskBasedTaskId).checkTaskBasedTaskEnd();
+		return result;
+	}
+}
---
+++ src/edu/boun/edgecloudsim/utils/TaskBasedTask.java
@@ -0,0 +1,152 @@
+package edu.boun.edgecloudsim.utils;
+
+import java.util.*;
+
+public class TaskBasedTask {
+	private int taskBasedTaskId;
+	private int numSubTask;
+	// map from the TaskPropertyId to the inner index id
+	private Map<Integer, Integer>propertyId2Index;
+	private Map<Integer, Integer>index2PropertyId;
+	private boolean[] submitted;
+	// finished used to check if the task has ended
+	private boolean[] finished;
+	/*
+	 * dependency[0][1] = 1 means task 0 is dependent on task 1
+	 * 	 0 1
+	 * 0 
+	 * 1
+	 */
+	private int[][] dependency;
+	/*
+	 * initial: if dependency[0][1] = 1, dependency_met[0][1] = 0
+	 * 			otherwise, dependency_met[0][1] = 1
+	 * dependency_met[0][1] = 1 means dependency for 0 is met considering 1
+	 */
+	private int[][] dependency_met;
+	
+
+	public TaskBasedTask(int _num, int _taskBasedTaskId) {
+		numSubTask = _num;
+		taskBasedTaskId = _taskBasedTaskId;
+		propertyId2Index = new HashMap<Integer, Integer>();
+		index2PropertyId = new HashMap<Integer, Integer>();
+		submitted = new boolean[numSubTask];
+		finished = new boolean[numSubTask];
+		for (int i=0; i<numSubTask; i++) {
+			submitted[i] = false; 
+			finished[i] = false; 
+		}
+		/*
+		 * Initialize the task with no dependency
+		 * dependencies are added later 
+		 */
+		dependency = new int[numSubTask][numSubTask];
+		dependency_met = new int[numSubTask][numSubTask];
+		for (int i=0; i < numSubTask; i++) {
+			for (int j=0; j<numSubTask; j++) {
+				dependency[i][j] = 0;
+				dependency_met[i][j]= 1; 
+			}
+		}
+	}
+	
+	/*
+	/*
+	 * Add a dependency dependencyTaskId for task taskId
+	 */
+	public void addDependency(int id, int id_dependency) {
+		//int task_index = getTaskIndex(taskId);
+		//int dependency_index = getTaskIndex(dependencyTaskId);
+		int taskId = propertyId2Index.get(id);
+		int dependencyTaskId = propertyId2Index.get(id_dependency);
+		dependency[taskId][dependencyTaskId] = 1;
+		dependency_met[taskId][dependencyTaskId] = 0;
+	}
+	
+	
+	public void addSubTaskIdList(int[] subTaskIdList) {
+		for (int i=0; i<subTaskIdList.length; i++) {
+			propertyId2Index.put(subTaskIdList[i], i);
+			index2PropertyId.put(i, subTaskIdList[i]);
+		}
+		
+	}
+	
+	/*
+	 * taskId: the index in subtaskLookUpTable
+	 * remove dependency when a task finished
+	 */
+	private void removeDependency(int taskPropertyId) {
+		int task_index = propertyId2Index.get(taskPropertyId);
+		for (int i=0; i<numSubTask; i++) {
+			dependency_met[i][task_index] = 1;
+		}
+	}
+
+	/*
+	 * return true: dependency has been met
+	 * return false: dependencies has not been met
+	 */
+	private boolean checkDependency(int index) {
+		int task_index = index;
+		boolean flag = true;
+		for (int i=0; i<numSubTask; i++) {
+			if (dependency_met[task_index][i] == 0) {
+				flag = false;
+			}
+		}
+
+		return flag;
+	}
+	
+
+	public List<Integer> getTaskToSubmit(int finishedTaskPropertyId) {
+		List<Integer> tasktoSubmit = new ArrayList<Integer>();
+		removeDependency(finishedTaskPropertyId);
+		// make sure we have set all the task to submitted
+		int index_submitted = propertyId2Index.get(finishedTaskPropertyId);
+		submitted[index_submitted] = true;
+		finished[index_submitted] = true;
+		for (int index=0; index<numSubTask; index++) {
+			// check if the dependencies has been met and whether the task has been submitted
+			if (checkDependency(index) && (submitted[index] == false)) {
+				tasktoSubmit.add(index2PropertyId.get(index));
+				submitted[index] = true; 
+			}
+		}
+		
+		return tasktoSubmit;
+	}
+	
+	public List<Integer> getInitialTaskToSubmit(){
+		List<Integer> tasktoSubmit = new ArrayList<>();
+		for (int index=0; index<numSubTask; index++) {
+			// check whether dependencies has been met and whether the task has been submitted
+			if (checkDependency(index) && (submitted[index] == false)) {
+				tasktoSubmit.add(index2PropertyId.get(index));
+				//submitted[index] = true; 
+			}
+		}
+		return tasktoSubmit;
+	}
+	
+	public boolean checkReadySubmit(int taskPropertyId) {
+		int taskIndex = propertyId2Index.get(taskPropertyId);
+		if (checkDependency(taskIndex) && submitted[taskIndex] == false) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public boolean checkTaskBasedTaskEnd() {
+		boolean flag = true;
+		for (int i=0; i<numSubTask; i++) {
+			if (finished[i]== false) {
+				flag = false;
+			}
+		}
+		return flag;
+	}
+}
